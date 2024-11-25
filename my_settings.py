754585917832c86b449d53278a380e8c69289dca
elisabbat <168416899+elisabbat@users.noreply.github.com>
2024-11-25 16:20:26 +0100

    Add files via upload

---
+++ my_settings.py
@@ -0,0 +1,15 @@
+#settings file 
+#goal: import libraries successfully, create a path like settings.py in sedos data_adapter
+
+
+import logging
+import os
+import pathlib
+
+import numpy as np
+import pandas as pd
+import math
+
+import datetime
+import json
+from typing import Union
